38434ac7564b5e65ea63edceef271b02e2110ac5
Sean Bradley <seanwasere@gmail.com>
2026-06-13 09:09:56 +0100

    Add Grafana CloudWatch monitoring

--- terraform/aws/ecs.tf
+++ terraform/aws/ecs.tf
@@ -1,3 +1,8 @@
+resource "aws_cloudwatch_log_group" "ecs" {
+  name              = "/ecs/ads-platform"
+  retention_in_days = 7
+}
+
 resource "aws_ecs_cluster" "main" {
   name = "ads-platform-cluster"
 }
@@ -15,9 +20,18 @@ resource "aws_ecs_task_definition" "app" {
   container_definitions = jsonencode([
     {
       name  = "ads-platform"
-      image = "190934385828.dkr.ecr.eu-west-2.amazonaws.com/ads-platform:latest"
+      image = "190934385828.dkr.ecr.eu-west-2.amazonaws.com/ads-platform:e9bce3f40a2628c87d45b8d0808e6b9ff32e6e9f"
 
       essential = true
+
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          "awslogs-group"         = aws_cloudwatch_log_group.ecs.name
+          "awslogs-region"        = "eu-west-2"
+          "awslogs-stream-prefix" = "ecs"
+        }
+      }
 
       portMappings = [
         {
